0327df66c4c7e940c02e003fe039ce30740efc29
Hieuzest <girkirin@hotmail.com>
2024-03-17 21:45:46 +0800

    feat: adapt to official admin, support builtinPermissions

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-import { Command, Context, deepEqual, Dict, Logger, Primary, remove, Schema } from 'koishi'
-
-declare module 'koishi' {
-
-  interface Tables {
-    permissions: PermissionGroup
-  }
-}
-
-interface PermissionGroup {
-  id: Primary
-  name: string
-  permissions: string[]
-}
-
-const logger = new Logger('permissions')
+import { Context, Schema } from 'koishi'
+import { } from '@koishijs/plugin-admin'
 
 function parsePlatform(target: string): [platform: string, id: string] {
   const index = target.indexOf(':')
@@ -23,57 +9,19 @@ function parsePlatform(target: string): [platform: string, id: string] {
 }
 
 export class Permissions {
-  disposables: Dict<() => void> = {}
-
   constructor(public ctx: Context, config: Permissions.Config) {
-    ctx.i18n.define('zh', require('./locales/zh.yml'))
+    ctx.i18n.define('zh', require('./locales/zh'))
 
-    ctx.model.extend('permissions', {
-      id: 'primary',
-      name: 'string',
-      permissions: 'list',
-    }, {
-      autoInc: true,
-    })
+    config.builtinPermissions.forEach(
+      perm => perm.enabled && perm.patterns.forEach(
+        pattern => ctx.permissions.define(pattern, {
+          depends: perm.depends,
+          inherits: perm.inherits,
+        }),
+      ),
+    )
 
-    ctx.schema.extend('command', Schema.object({
-      authority: Schema.natural().description('指令的权限等级。').default(1),
-      permissions: Schema.array(Schema.string()),
-      dependencies: Schema.array(Schema.string()),
-    }))
-
-    function updateCommand(command: Command | string) {
-      if (typeof command === 'string') command = ctx.$commander.get(command)
-      const name = `command.${command.name}`
-
-      ctx.permissions._depends.delete(name)
-      ctx.permissions._inherits.delete(name)
-
-      remove(ctx.scope.disposables, ctx.permissions.config(name, command.config))
-    }
-
-    ctx.$commander._commandList.forEach(updateCommand)
-    ctx.on('command-added', updateCommand)
-
-    ctx.on('config', function _() {
-      console.log('config', arguments, this)
-    })
-
-    ctx.on('internal/before-update', (state, config) => {
-      if (state.runtime.name !== 'CommandManager') return
-      const resolved = state.runtime.config
-      const modified: Record<string, boolean> = Object.create(null)
-      const checkPropertyUpdate = (key: string) => modified[key] ??= !deepEqual(state.config[key], resolved[key])
-
-      for (const key in { ...state.config, ...resolved }) {
-        if (!(key in modified) && checkPropertyUpdate(key)) {
-          logger.debug(`update command config: ${key}`)
-          updateCommand(key)
-        }
-      }
-    })
-
-    ctx.command('perm <...perms:string>')
+    ctx.command('perm <...perms:string>', { authority: 3 })
       .option('user', '-u <user:user>')
       .option('currentUser', '-U')
       .option('channel', '-c <channel:channel>')
@@ -82,7 +30,9 @@ export class Permissions {
       .option('delete', '-d')
       .action(async ({ session, options }, ...perms) => {
         if (options.delete && options.group && !perms.length) {
-          await this.deleteGroup(`group.${options.group}`)
+          const group = this.ctx.admin.groups.find(x => x.name === target)
+          if (!group) return session.text('.notfound')
+          await ctx.admin.deleteGroup(group.id)
           return session.text('.deleted')
         }
 
@@ -120,7 +70,7 @@ export class Permissions {
       const channel = await this.ctx.database.getChannel(...parsePlatform(target.slice(1)))
       return channel?.permissions
     } else {
-      const group = (await this.ctx.database.get('permissions', { name: target }))?.[0]
+      const group = this.ctx.admin.groups.find(x => x.name === target)
       return group?.permissions
     }
   }
@@ -145,39 +95,42 @@ export class Permissions {
         { permissions: [...permissions] },
       )
     } else {
-      const group = (await this.ctx.database.get('permissions', { name: target }))?.[0]
-        ?? (await this.ctx.database.create('permissions', { name: target }))
+      let group = this.ctx.admin.groups.find(x => x.name === target)
+      if (!group) {
+        const id = await this.ctx.admin.createGroup(target)
+        group = this.ctx.admin.groups.find(x => x.id === id)
+      }
       const permissions = new Set(group.permissions)
       set.forEach(permissions.add.bind(permissions))
       unset.forEach(permissions.delete.bind(permissions))
-      await this.ctx.database.set('permissions', { name: target }, { permissions: [...permissions] })
-      await this.#updateGroup(target, [...permissions])
+      await this.ctx.admin.updateGroup(group.id, [...permissions])
     }
     return true
-  }
-
-  async deleteGroup(name: string) {
-    await this.ctx.database.remove('permissions', { name })
-    this.disposables[name]?.()
-    delete this.disposables[name]
-  }
-
-  async #updateGroup(name: string, permissions?: string[]) {
-    if (!permissions) {
-      permissions = (await this.ctx.database.get('permissions', { name }))?.[0]?.permissions
-    }
-    if (!permissions) return
-    this.disposables[name]?.()
-    this.disposables[name] = this.ctx.permissions.define(name, permissions)
   }
 }
 
 export namespace Permissions {
   export const inject = ['database']
 
-  export interface Config {}
+  export interface BuiltinPermOptions {
+    patterns: string[]
+    enabled: boolean
+    depends?: string[]
+    inherits?: string[]
+  }
 
-  export const Config: Schema<Config> = Schema.object({})
+  export interface Config {
+    builtinPermissions: BuiltinPermOptions[]
+  }
+
+  export const Config: Schema<Config> = Schema.object({
+    builtinPermissions: Schema.array(Schema.object({
+      patterns: Schema.array(String).role('table'),
+      enabled: Schema.boolean().default(true),
+      depends: Schema.array(String).role('table'),
+      inherits: Schema.array(String).role('table'),
+    })).default([]),
+  })
 }
 
 export default Permissions
